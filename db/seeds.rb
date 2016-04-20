12e8cf14df7a628b576515fd1e0f203cf242d87a
Andreza Feu <andrezafeu@gmail.com>
2016-04-20 17:51:14 -0400

    Create users on seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+user1 = User.create(name: 'Andreza', email: 'andreza@example.com')
+user2 = User.create(name: 'Faraz', email: 'mumbairocks@example.com')
+user3 = User.create(name: 'Nizar', email: 'ilovepizza@example.com')
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
