eabae7f86fd485a4c8366473af5ff51850bc8475
Andreza Feu <andrezafeu@gmail.com>
2016-04-20 18:07:03 -0400

    Create users and products erb files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160420211848) do
+ActiveRecord::Schema.define(version: 20160420215731) do
+
+  create_table "bids", force: :cascade do |t|
+    t.float    "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
