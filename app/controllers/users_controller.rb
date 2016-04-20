3aad83376ccbc730c8d88e2648af401bf422f7e4
Andreza Feu <andrezafeu@gmail.com>
2016-04-20 18:21:15 -0400

    Add users names to users index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,15 @@
 class UsersController < ApplicationController
+	def index
+		@users = User.order(name: :asc).limit(10)
+		render "index"
+	end
 	def show
 	end
 	def create
+		@the_user  = User.new(
+			:name => params[:user][:name],
+			:email => params[:user][:email]
+		)
 	end
 	def destroy
 	end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get '/products', to: 'products#index', as: :products
 
   get '/bids', to: 'bids#index', as: :bids
+
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
