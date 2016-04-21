57b4f812920998c357c919a0d6fc4f9cb2f77b51
Andreza Feu <andrezafeu@gmail.com>
2016-04-21 16:29:41 -0400

    Generate Bids controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160421053159) do
+ActiveRecord::Schema.define(version: 20160421133624) do
 
   create_table "bids", force: :cascade do |t|
     t.float    "value"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.integer  "product_id"
   end
 
   create_table "products", force: :cascade do |t|
